8edabe93b52ec0a5a1f482091d0fb10053b8ce23
KiranShny <ikiran1997>
2022-09-25 21:01:14 +0530

    Adds regex check for URL's

--- app/src/main/java/io/github/kiranshny/qrscanner/exts/CommonExts.kt
+++ app/src/main/java/io/github/kiranshny/qrscanner/exts/CommonExts.kt
@@ -4,9 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 
+private const val WEBSITE_REGEX =
+    "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
+
 fun String.launch(context: Context): Boolean {
     val intent = Intent(Intent.ACTION_VIEW, Uri.parse(this))
-    return if (intent.resolveActivity(context.packageManager) != null) {
+    return if (this.matches(Regex(WEBSITE_REGEX))) {
+        context.startActivity(intent)
+        true
+    } else if (intent.resolveActivity(context.packageManager) != null) {
         context.startActivity(intent)
         true
     } else {
