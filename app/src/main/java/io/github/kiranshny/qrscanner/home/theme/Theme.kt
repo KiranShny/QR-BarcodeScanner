b6f290a437af197277f9e0aed5c85a6a4ff3b92b
Ashokmani <ashok.ss@zohocorp.com>
2023-09-02 12:59:00 +0530

    Added support for Dynamic Color to the app. This allows users to personalize the colors of the app's interface to match their wallpaper.

--- app/src/main/java/io/github/kiranshny/qrscanner/home/theme/Theme.kt
+++ app/src/main/java/io/github/kiranshny/qrscanner/home/theme/Theme.kt
@@ -1,10 +1,14 @@
 package io.github.kiranshny.qrscanner.home.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 
 private val LightColors = lightColorScheme(
@@ -70,14 +74,17 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun QRScannerTheme(
-  useDarkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colors = when
+    {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
 
   MaterialTheme(
     colorScheme = colors,
